5bf0d0bf28ffb35e0cbf8fa7e2b0afc831c01491
Ira Miller <ira@elementalrain.com>
2011-09-18 09:32:06 -0600

    Added the MaxMind GEOIP library
    The MaxMind GEOIP library is a third party LGPL set of source files
    which have been packaged into a separate library for use in portions of
    the system which will identify the location of where an IP block is
    assigned to.
    
    No modifications have been made to this source code, the project files
    where developed to build the source into a useable library.

---
+++ source/Third Party/MaxMindGeoIP/Region.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+namespace MaxMind.GeoIP
+{
+	public class Region
+	{
+		public String countryCode;
+		public String countryName;
+		public String region;
+		public Region()
+		{
+		}
+		public Region(String countryCode, String countryName, String region)
+		{
+			this.countryCode = countryCode;
+			this.countryName = countryName;
+			this.region = region;
+		}
+		public String getcountryCode()
+		{
+			return countryCode;
+		}
+		public String getcountryName()
+		{
+			return countryName;
+		}
+		public String getregion()
+		{
+			return region;
+		}
+	}
+}
